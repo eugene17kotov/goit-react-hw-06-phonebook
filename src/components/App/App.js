460eff65901db19d417ba635faaaedfa62440006
Yevhenii Kobets <eu.kobets@gmail.com>
2022-09-22 12:12:52 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
